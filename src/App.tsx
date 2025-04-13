dfc576bb82bc2fe73b05207eb8dc50b7513a15c0
Babak Mehrabi <b.mehrabi.91@gmail.com>
2025-04-13 09:52:38 +0200

    Read todos list from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import List from "./todo/list";
@@ -6,17 +6,25 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Header from "./header/header";
 import CreatePage from "./todo/create";
 import {TodoType} from "./models/Todo";
+import axios from "axios";
 
 function App() {
-    const todos: TodoType[] = [
-        {id: 1, title: 'firstTodo', isImportant: true, hasDone: false},
-        {id: 2, title: 'secondTodo', isImportant: false, hasDone: false},
-    ]
+    const API = 'http://localhost:8000/todos';
+    const [data, setData] = useState<TodoType[]>([]);
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await axios(`${API}`);
+            setData(result.data);
+        };
+
+        fetchData();
+
+    }, [setData, API]);
   return (
       <BrowserRouter>
           <Header />
           <Routes>
-              <Route path="/" element={<List todos={todos} />} />
+              <Route path="/" element={<List todos={data} />} />
               <Route path="/create" element={<CreatePage />} />
           </Routes>
       </BrowserRouter>
