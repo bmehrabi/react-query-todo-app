5fe869cefc817a1e5a2881ca323756106920f2b6
Babak Mehrabi <b.mehrabi.91@gmail.com>
2025-04-18 16:22:05 +0200

    Use vitest for running tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import {configDefaults} from "vitest/config";
 
 export default defineConfig({
     plugins: [react()],
     server: {
         port: 3000,
+    },
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './setupTests.ts',
+        exclude: [...configDefaults.exclude, 'e2e/**']
     }
 });
